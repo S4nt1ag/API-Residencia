76f66fd8ef1e38e56a3a2bb143e588c9ff30580a
Santiago <gabrielsantiagoneves2@gmail.com>
2023-05-11 15:57:50 -0300

    criação dos arquivos com FK e PK

---
+++ biblioteca/src/main/java/com/residencia/biblioteca/entities/Livro.java
@@ -0,0 +1,118 @@
+package com.residencia.biblioteca.entities;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	@Column(name = "codigolivro")
+	private Integer codigoLivro;
+
+	@Column(name = "nomelivro")
+	private String nomeLivro;
+
+	@Column(name = "nomeautor")
+	private String nomeAutor;
+
+	@Column(name = "datalancamento")
+	private Date dataLancamento;
+
+	@Column(name = "codigoisbn")
+	private Long codigoISBN;
+
+	@OneToMany(mappedBy = "livro")
+	private List<Emprestimo> emprestimos;
+
+	@OneToOne
+	@JoinColumn(name = "codigoeditora", referencedColumnName = "codigoeditora")
+	private Editora editora;
+
+	public Integer getCodigoLivro() {
+		return codigoLivro;
+	}
+
+	public void setCodigoLivro(Integer codigoLivro) {
+		this.codigoLivro = codigoLivro;
+	}
+
+	public String getNomeLivro() {
+		return nomeLivro;
+	}
+
+	public void setNomeLivro(String nomeLivro) {
+		this.nomeLivro = nomeLivro;
+	}
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+
+	public void setNomeAutor(String nomeAutor) {
+		this.nomeAutor = nomeAutor;
+	}
+
+	public Date getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(Date dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
+	public Long getCodigoISBN() {
+		return codigoISBN;
+	}
+
+	public void setCodigoISBN(Long codigoISBN) {
+		this.codigoISBN = codigoISBN;
+	}
+
+	public List<Emprestimo> getEmprestimos() {
+		return emprestimos;
+	}
+
+	public void setEmprestimos(List<Emprestimo> emprestimos) {
+		this.emprestimos = emprestimos;
+	}
+
+	public Editora getEditora() {
+		return editora;
+	}
+
+	public void setEditora(Editora editora) {
+		this.editora = editora;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigoLivro);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		return Objects.equals(codigoLivro, other.codigoLivro);
+	}
+
+}
