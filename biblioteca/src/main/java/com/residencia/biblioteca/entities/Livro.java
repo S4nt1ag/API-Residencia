3383fedd82d067dcc9652dc9d1e2092ae7bc6478
Santiago <gabrielsantiagoneves2@gmail.com>
2023-05-12 17:02:17 -0300

    Criação Service

--- biblioteca/src/main/java/com/residencia/biblioteca/entities/Livro.java
+++ biblioteca/src/main/java/com/residencia/biblioteca/entities/Livro.java
@@ -10,8 +10,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -38,7 +38,7 @@ public class Livro {
 	@OneToMany(mappedBy = "livro")
 	private List<Emprestimo> emprestimos;
 
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name = "codigoeditora", referencedColumnName = "codigoeditora")
 	private Editora editora;
 
---
+++ biblioteca/src/main/java/com/residencia/biblioteca/services/AlunoService.java
@@ -0,0 +1,43 @@
+package com.residencia.biblioteca.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.residencia.biblioteca.entities.Aluno;
+import com.residencia.biblioteca.repositories.AlunoRepository;
+
+@Service
+public class AlunoService {
+	
+	@Autowired
+	AlunoRepository alunoRepository;
+	
+	
+	public List<Aluno> getAllAlunos() {
+		return alunoRepository.findAll();
+	}
+	
+	public Aluno getAlunoById(Integer id) {
+		return alunoRepository.findById(id).orElse(null);
+	}
+	
+	public Aluno saveAluno(Aluno aluno) {
+		return alunoRepository.save(aluno);
+	}
+	
+	public Aluno updateAluno(Aluno aluno, Integer id) {
+		return alunoRepository.save(aluno);
+	}
+	public Boolean deleteAluno(Integer id) {
+		alunoRepository.deleteById(id);
+		Aluno alunoDeletado = alunoRepository.findById(id).orElse(null);
+		if(alunoDeletado == null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
